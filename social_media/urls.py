2c32b9e0f33e972c65a99a22199d041b2b282b19
CristNord <cristnordgren@gmail.com>
2022-06-07 16:21:21 +0200

    updated register url only

--- social_media/urls.py
+++ social_media/urls.py
@@ -13,13 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 from social_app import views
+from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    url(r'^$', views.login, name='login'), 
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
-    url(r'^register', views.register, name='register'),
-    # url(r'^index', views.index),
-]
+    url(r'^users/', include('users.urls')),
+    url('register/', views.register, name='register'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ users/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from .forms import UserRegisterForm, UserProfilePicForm
+# Create your views here.
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserRegisterForm(data=request.POST)
+        profile_form = UserProfilePicForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            # if 'profile_pic' in request.FILES:
+            #     profile.profile_pic = request.FILES['profile_pic']
+
+            # profile.save()
+            image = request.FILES['profile_pic']
+            if image:
+                filename = FileSystemStorage().save('profile_pics/' + image.name, image)
+                profile.profile_pic = filename
+            profile.save()
+            
+            registered = True
+
+        else:
+            print(user_form.errors, profile_form.errors)
+
+    else:
+        user_form = UserRegisterForm()
+        profile_form = UserProfilePicForm()
+
+    return render(request, 'registration.html', 
+                  {'user_form':user_form,
+                  'profile_form':profile_form,
+                  'registered':registered
+                  })
+
+
+def login(request):
+    return render(request, 'users/login.html')
