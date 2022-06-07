23628f0581eb1e08e3a1ac416a938b7a76d87e0d
danilomirkovic <51163989+danilomirkovic@users.noreply.github.com>
2022-06-07 13:33:27 +0200

    Created the base, login and registration

--- social_app/views.py
+++ social_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def base(request):
+    return render(request, 'social_app/base.html')
+
+def register(request):
+    return render(request, 'social_app/registration.html')
+
+def index(request):
+    return render(request, 'social_app/index.html')
