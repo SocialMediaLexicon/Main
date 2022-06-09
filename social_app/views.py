fac5ef397b06567674539c0f12a8d057b1e30a19
Osama Chaban <osama.chaban82@gmail.com>
2022-06-09 15:51:23 +0200

    Added POST method for add_post view, fixed PostForm
    Problem with Media not being saved, working on a solution

--- social_app/views.py
+++ social_app/views.py
@@ -1,17 +1,20 @@
 from turtle import setundobuffer
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from users.models import Profile
+from social_app.models import Post
 from .forms import PostForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
-def base(request):
-    return render(request, 'social_app/base.html')
+# def base(request):
+#     return render(request, 'social_app/base.html')
 
-def register(request):
-    return render(request, 'social_app/registration.html')
+# def register(request):
+#     return render(request, 'social_app/registration.html')
 
-def index(request):
-    return render(request, 'social_app/index.html')
+# def index(request):
+#     return render(request, 'social_app/index.html')
 
 def add_post(request):
     if request.method == 'GET':
@@ -19,6 +22,12 @@ def add_post(request):
         form = PostForm()
         form.author = request.user
         return render(request, 'social_app/add_post.html', {'form': form }) 
-    else:
-        # request
-        return #
+    elif request.method == 'POST':
+        posts = Post.objects.all().order_by('created_on')
+        form = PostForm(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.author = Profile.objects.get(user=request.user)
+            print(request.user)
+            new_post.save()
+            return redirect('http://127.0.0.1:8000/login/', posts)
