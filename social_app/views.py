f7e9cdbe09cabd3c95e8f4d70318dbe4f1026586
danilomirkovic <51163989+danilomirkovic@users.noreply.github.com>
2022-06-10 09:53:58 +0200

    Post functionality

--- social_app/views.py
+++ social_app/views.py
@@ -1,6 +1,8 @@
 from turtle import setundobuffer
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import PostForm
+from users.models import Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -12,13 +14,44 @@ def register(request):
 
 def index(request):
     return render(request, 'social_app/index.html')
+    
+    
+@login_required
+def user_profile(request):
+    queryset = Post.objects.filter(status=1)
 
+    post_dict = {'post_list': queryset}
+    print(post_dict)
+    return render(request, 'users/user_profile.html', context=post_dict)
 def add_post(request):
-    if request.method == 'GET':
-        # do stuff
-        form = PostForm()
-        form.author = request.user
-        return render(request, 'social_app/add_post.html', {'form': form }) 
+    
+    if request.method == 'POST':
+
+        post_form = PostForm(data=request.POST, files=request.FILES)
+    
+        if post_form.is_valid():
+
+            blogpost = post_form.save(commit=False)
+
+            blogpost.author = request.user
+
+            blogpost.save()
+
+            return redirect('login')
+
+        # else:
+
+        #     print(post_form.error)
+
     else:
-        # request
-        return #
+
+        post_form = PostForm()
+    return render(request, 'social_app/add_post.html', {'form': post_form }) 
+
+    # if request.method == 'GET':
+    #     if request.user.is_authenticated():
+    #         profile = request.user.username
+    #         print(profile) 
+            # form.author = request.user.username
+    # print(form.author)
+
